cd732fc5da58136142824a49fb7310e3489a591b
Susan Njeri Githaiga <susangithaiga26@gmail.com>
2023-07-20 11:00:37 +0300

    Created an etch-a-sketch app

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+from turtle import Turtle, Screen
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+spice = Turtle()
+screen = Screen()
 
+# start listening
+screen.listen()
+# bind a function to a keystroke
+def move_foward():
+    spice.forward(10)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen.onkey(key= "space", fun=move_foward)
+screen.exitonclick()
---
+++ sketch.py
@@ -0,0 +1,46 @@
+ # w = Fowards
+ # s= backwards
+ # a = counter-clockwise
+ # d =clockwise
+ # c - clear drawing
+
+from turtle import Turtle, Screen
+
+sketch = Turtle()
+screen = Screen()
+# start listening
+screen.listen()
+# bind a function to a keystroke
+# prompt user to enter a keystroke
+# convert user input to lower()
+
+
+def move_foward():
+    sketch.forward(10)
+def move_backward():
+    sketch.backward(10)
+
+def turn_left():
+    # new_heading = sketch.heading() + 10
+    # sketch.setheading((new_heading))
+    sketch.left(10)
+
+def turn_right():
+    sketch.right(10)
+
+def clear():
+    sketch.clear()
+    # bring turtle back to starting point
+    sketch.penup()
+    sketch.home()
+    sketch.pendown()
+
+# movements
+screen.listen()
+screen.onkey(move_foward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear, "c")
+
+screen.exitonclick()
